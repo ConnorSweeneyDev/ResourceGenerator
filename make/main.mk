502cd804f91c62bad216bc23c256ac0b2376cf63
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-08-08 22:40:29 +0100

    Better organisation

--- make/main.mk
+++ make/main.mk
@@ -1,7 +1,6 @@
 include make/platform.mk
 include make/flags.mk
 include make/files.mk
-include make/utility.mk
 
 build: $(OUTPUT_FILE)
 prepare: directories
@@ -9,3 +8,4 @@ utility: compile_commands clang-format
 clean: delete
 
 include make/build.mk
+include make/utility.mk
