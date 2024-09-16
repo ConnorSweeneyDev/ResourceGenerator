f74b1fbf2eeb10315c9bb23fd3fca07e2a6324ee
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-09-16 17:06:23 +0100

    Better variable name

--- make/build.mk
+++ make/build.mk
@@ -1,7 +1,7 @@
 directories:
 	@if [ ! -d $(BINARY_DIRECTORY) ]; then mkdir -p $(BINARY_DIRECTORY); $(ECHO) "Write | $(BINARY_DIRECTORY)"; fi
 	@if [ ! -d $(OBJECT_DIRECTORY) ]; then mkdir -p $(OBJECT_DIRECTORY); $(ECHO) "Write | $(OBJECT_DIRECTORY)"; fi
-	@if [ ! -d $(TARGET_DIRECTORY) ]; then mkdir -p $(TARGET_DIRECTORY); $(ECHO) "Write | $(TARGET_DIRECTORY)"; fi
+	@if [ ! -d $(TARGET_PLATFORM_DIRECTORY) ]; then mkdir -p $(TARGET_PLATFORM_DIRECTORY); $(ECHO) "Write | $(TARGET_PLATFORM_DIRECTORY)"; fi
 
 $(OBJECT_DIRECTORY)/%.o: $(PROGRAM_SOURCE_DIRECTORY)/%.cpp $(PROGRAM_INCLUDE_DIRECTORY)/%.hpp
 	@$(CXX) $(CXXFLAGS) $(WARNINGS) $(INCLUDES) $(SYSTEM_INCLUDES) -c $< -o $@
--- make/files.mk
+++ make/files.mk
@@ -1,11 +1,11 @@
 ifeq ($(UNAME), Windows)
   ECHO := echo -e
-  TARGET_DIRECTORY := binary/windows
-  OUTPUT_FILE := $(TARGET_DIRECTORY)/ResourceLoader.exe
+  TARGET_PLATFORM_DIRECTORY := binary/windows
+  OUTPUT_FILE := $(TARGET_PLATFORM_DIRECTORY)/ResourceLoader.exe
 else ifeq ($(UNAME), Linux)
   ECHO := echo
-  TARGET_DIRECTORY := binary/linux
-  OUTPUT_FILE := $(TARGET_DIRECTORY)/ResourceLoader.out
+  TARGET_PLATFORM_DIRECTORY := binary/linux
+  OUTPUT_FILE := $(TARGET_PLATFORM_DIRECTORY)/ResourceLoader.out
 endif
 
 PROGRAM_SOURCE_DIRECTORY := program/source
