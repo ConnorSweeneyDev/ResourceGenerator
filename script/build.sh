ef5edb21f330dc3298d8212a904b28c67f80d1d2
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-09-16 12:45:07 +0100

    Better build system

--- script/build.sh
+++ script/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 CPU_COUNT_MULTIPLIER=$(python3 -c 'import multiprocessing as mp; print(int(mp.cpu_count() * 1.5))')
+FLAGS="-s -f make/main.mk -j $CPU_COUNT_MULTIPLIER DEBUG=0"
 
-make -f make/main.mk prepare -j $CPU_COUNT_MULTIPLIER && make -f make/main.mk build -j $CPU_COUNT_MULTIPLIER
+UTILITY="utility $FLAGS"
+PREPARE="prepare $FLAGS"
+BUILD="build $FLAGS"
+make $PREPARE && make $BUILD
