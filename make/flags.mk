3a415ff3919aff8a652d19d7758b9664e2ded18a
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-10-08 16:58:56 +0100

    Better g++ flags

--- make/flags.mk
+++ make/flags.mk
@@ -1,8 +1,8 @@
 CXX := g++
 ifeq ($(DEBUG), 1)
-  CXXFLAGS := -g3 -MD -MP -O2 -std=c++17 -DDEBUG -D_FORTIFY_SOURCE=2 -fno-common -fstack-protector-strong
+  CXX_FLAGS := -ggdb3 -MD -MP -O2 -std=c++17 -DDEBUG -D_FORTIFY_SOURCE=2 -fno-common -fstack-protector-strong
 else
-  CXXFLAGS := -s -O3 -std=c++17 -DNDEBUG -fno-common -fstack-protector-strong -ffunction-sections -fdata-sections -flto=auto -Wl,--gc-sections
+  CXX_FLAGS := -s -O3 -std=c++17 -DNDEBUG -fno-common -fstack-protector-strong -ffunction-sections -fdata-sections -flto=auto -Wl,--gc-sections
 endif
 
 WARNINGS := -Wall -Wextra -Wpedantic -Wconversion -Wshadow -Wcast-qual -Wcast-align -Wfloat-equal -Wlogical-op -Wduplicated-cond -Wshift-overflow=2 -Wformat=2
