fbf07f799fd3af9b3ab45d81b13a213c6c569467
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-10-09 03:50:01 +0100

    Better build scripts

--- script/run.bat
+++ script/run.bat
@@ -1,3 +1,9 @@
 @ECHO OFF
 
-START pwsh -NoExit -Command "./binary/windows/ResourceLoader.exe _resource assets/vertex_shader.glsl assets/lamp.png assets/background1.png output.cpp && ./binary/windows/ResourceLoader.exe _resource assets/vertex_shader.glsl assets/lamp.png assets/background1.png output.hpp"
+IF "%1" == "-wezterm" (
+  SET TERM=wezterm cli spawn --cwd %CD% pwsh -Command
+) ELSE (
+  SET TERM=pwsh -Command
+)
+
+%TERM% "./binary/windows/ResourceLoader.exe _resource assets/vertex_shader.glsl assets/lamp.png assets/background1.png output.cpp && ./binary/windows/ResourceLoader.exe _resource assets/vertex_shader.glsl assets/lamp.png assets/background1.png output.hpp"
