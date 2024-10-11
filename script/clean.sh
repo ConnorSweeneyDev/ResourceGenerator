8547e222d455dfe4864cd934726c86ca305119e0
Connor Sweeney <connor.sweeney1234@gmail.com>
2024-10-11 23:54:14 +0100

    More succinct batch files

--- script/clean.sh
+++ script/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 FLAGS="-f make/main.mk"
-
 CLEAN="clean $FLAGS"
+
 make $CLEAN
--- script/run.bat
+++ script/run.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-SET COMMAND=binary/windows/ResourceLoader.exe _resource assets/vertex_shader.glsl assets/lamp.png assets/background1.png output
-IF "%1" == "-wezterm" (wezterm cli spawn --cwd %CD% pwsh -Command "./%COMMAND%.cpp && ./%COMMAND%.hpp")
-IF "%1" == "" (cmd /C "start %COMMAND%.cpp && start %COMMAND%.hpp")
+SET COMMAND=pwsh -Command "./binary/windows/ResourceLoader.exe _resource assets/vertex_shader.glsl assets/lamp.png assets/background1.png output.cpp && ./binary/windows/ResourceLoader.exe _resource assets/vertex_shader.glsl assets/lamp.png assets/background1.png output.hpp"
+IF "%1" == "-wezterm" (wezterm cli spawn --cwd %CD% %COMMAND%)
+IF "%1" == "" (START %COMMAND%)
